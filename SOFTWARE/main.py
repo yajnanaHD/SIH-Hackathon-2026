e2d571112f987d9abfe3c5b2c7310f5692e100cd
YajnanaHD <sharmadhananjay1904@gmail.com>
2026-08-25 23:11:03 +0530

    Made changes to the main.py file and made changes to the SETUP file

--- SOFTWARE/main.py
+++ SOFTWARE/main.py
@@ -2,5 +2,13 @@ from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt") #loaded a pre trained model
 
-results = model.predict(source = 0, show = True, classes = [0]) 
+#results = model.predict(source = 0, show = True, classes = [0]) 
 #source 0 is a webcam, show = True will display the webcam feed with predictions, classes = [0] will filter for the person
+
+#now attempting tracking on a video 
+
+results = model.track(source=0, show=True, persist=True, classes=[0])
+
+for r in results:
+    print(r.boxes.id)
+    print(r.boxes.xywh)
